d414eb0d4e7b4de84d97b0990b1cf1fb2599266e
Madhab Paul <madhab12paul@gmail.com>
2021-05-26 00:52:52 +0530

    lesson 1

---
+++ script.js
@@ -0,0 +1,26 @@
+//Scene
+const scene = new THREE.Scene();
+
+// Cube object or MESH
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const size = {
+    width: 800,
+    height: 600
+}
+//camera
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+camera.position.z=3;
+scene.add(camera);
+
+//renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+   canvas: canvas
+})
+renderer.setSize(size.width, size.height);
+
+renderer.render(scene, camera);
